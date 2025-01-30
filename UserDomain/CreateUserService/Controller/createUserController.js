72e367f0f10d96fe4efdf6713074bf078f56cef3
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-01-29 20:46:18 -0500

    Update createUserController.js

--- UserDomain/CreateUserService/Controller/createUserController.js
+++ UserDomain/CreateUserService/Controller/createUserController.js
@@ -1,10 +1,10 @@
-var Usuario = require('../models/User');
-var Usuario_invitacion = require('../models/User_invitation');
-var Usuario_amigo = require('../models/User_friend');
+var Usuario = require('../model/User'); // Asegurar que el modelo se importa correctamente
+var Usuario_invitacion = require('../model/User_invitation');
+var Usuario_amigo = require('../model/User_friend');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 var jwt = require('../helpers/jwt');
-const { uniqueUsernameGenerator  } = require("unique-username-generator");
+const { uniqueUsernameGenerator } = require("unique-username-generator");
 
 var path = require('path');
 var fs = require('fs');
@@ -13,36 +13,42 @@ var ejs = require('ejs');
 var nodemailer = require('nodemailer');
 var smtp = require('nodemailer-smtp-transport');
 
-
-const create_user = async function(req,res){
+const create_user = async function (req, res) {
     console.log(req.body);
     let data = req.body;
 
-    let users = await User.find({email:data.email});
+    // Verificar si el usuario ya existe
+    let users = await Usuario.find({ email: data.email }); // ✅ Corrección aquí
 
-    if(users.length == 0){
+    if (users.length == 0) {
         let usersnames = [];
-        usersnames.push(data.names+''+data.surnames);
+        usersnames.push(data.nombres + '' + data.apellidos);
 
         const config = {
             dictionaries: [usersnames],
             separator: '',
             style: 'capital',
             randomDigits: 3
-        }
+        };
 
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            bcrypt.hash(data.password, salt, async function(err, hash) {
-                // Store hash in your password DB.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            bcrypt.hash(data.password, salt, async function (err, hash) {
+                if (err) {
+                    return res.status(500).send({ message: "Error al encriptar la contraseña" });
+                }
                 data.password = hash;
-                data.username = '@'+uniqueUsernameGenerator(config);
-                let user = await User.create(data);
-                res.status(200).send({data:user});
+                data.username = '@' + uniqueUsernameGenerator(config);
+
+                let user = await Usuario.create(data); // ✅ Corrección aquí
+                res.status(200).send({ data: user });
             });
         });
-    }else{
-        res.status(200).send({data:undefined,message: 'El correo electrónico ya existe'});
+    } else {
+        res.status(400).send({ message: 'El correo electrónico ya existe' });
     }
-}
+};
 
-
+// Exportar correctamente la función
+module.exports = {
+    create_user
+};
