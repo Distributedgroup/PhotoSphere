6b34c00682721bd9e3893e4eb4c5a02e22a565dd
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-01-30 18:41:13 -0500

    Update updateUserController.js

--- UserDomain/UpdateUserService/Controller/updateUserController.js
+++ UserDomain/UpdateUserService/Controller/updateUserController.js
@@ -1,27 +1,36 @@
-const update_user = async function(req,res){
-    if (req.user) {
-        var id = req.params['id'];
-        var data = req.body;
+const User = require('../Model/User'); // Ensure the model is imported correctly.
 
-        console.log(data);
+const update_user = async function(req, res) {
+    try {
+        const id = req.params['id'];
+        const data = req.body;
 
-        var user = await User.findByIdAndUpdate({_id:id},{
-            names: data.names,
-            surnames: data.surnames,
-            genrer: data.genrer,
-            birth: data.birth,
-            profession: data.profession,
-            telephone: data.telephone,
-            description: data.description
-        });
+        // Check if the user exists
+        let userExists = await User.findById(id);
+        if (!userExists) {
+            return res.status(404).send({ message: "User not found" });
+        }
 
-        res.status(200).send({data:user});
-    } else {
-        res.status(403).send({message: 'NoAccess'}); 
+        console.log("Data received for update:", data);
+
+        // Update user
+        let user = await User.findByIdAndUpdate(id, {
+            names: data.names || userExists.names,
+            surnames: data.surnames || userExists.surnames,
+            genrer: data.genrer || userExists.genrer,
+            birth: data.birth || userExists.birth,
+            profession: data.profession || userExists.profession,
+            telephone: data.telephone || userExists.telephone,
+            description: data.description || userExists.description
+        }, { new: true }); // `new: true` returns the updated document
+
+        res.status(200).send({ message: "User updated successfully", data: user });
+    } catch (error) {
+        console.error("Error updating user:", error);
+        res.status(500).send({ message: "Server error", error });
     }
-}
-
+};
 
 module.exports = {
     update_user
-}
+};
