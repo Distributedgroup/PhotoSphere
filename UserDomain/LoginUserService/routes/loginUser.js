f6f06968a3b3dffa64aef416f65374b2e90b1771
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-01-31 19:12:33 -0500

    Update loginUser.js

--- UserDomain/LoginUserService/routes/loginUser.js
+++ UserDomain/LoginUserService/routes/loginUser.js
@@ -1,12 +1,11 @@
 var express = require('express');
-var historiasController = require('../controllers/loginUserController');
+var loginUserController = require('../Controller/loginUserController');
 var auth = require('../middlewares/auth');
 var multiparty = require('connect-multiparty');
 var path = multiparty({uploadDir:'./uploads/stories'});
 
-
 var app = express.Router();
 
-app.post('/login_user',loginUserController.login_user);
+app.post('/login_user', loginUserController.login_user);
 
 module.exports = app;
