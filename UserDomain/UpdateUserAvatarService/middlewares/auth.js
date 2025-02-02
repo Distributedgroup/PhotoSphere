31cebb7fc869b7adce217a692bbf8fd5da513802
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-02 00:38:35 -0500

    Update auth.js

--- UserDomain/UpdateUserAvatarService/middlewares/auth.js
+++ UserDomain/UpdateUserAvatarService/middlewares/auth.js
@@ -2,28 +2,42 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var secret = '6M5X#D6%7Nh*!pkR3HL7F@Fdx';
 
-exports.auth = function(req,res,next){
-    if(!req.headers.authorization){
-        return res.status(403).send({message: 'NoHeadersError'}); 
+exports.auth = function(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(403).send({ message: 'NoHeadersError' });
     }
 
-    var token = req.headers.authorization.replace(/['"]+/g,'');
-
-    var segment = token.split('.');
+    var token = req.headers.authorization.replace(/['"]+/g, '');
     
-    if(segment.length != 3){
-        return res.status(403).send({message: 'InvalidToken'}); 
-    }else{
-        try {
-            var payload = jwt.decode(token,secret);
-            if(payload.exp <= moment().unix()) return res.status(403).send({message: 'TokenExpirado'}); 
-        } catch (error) {
-            console.log(error);
-            return res.status(403).send({message: 'ErrorToken'}); 
-        }
+    console.log("Token recibido:", token);
+
+    if (!token.startsWith("Bearer ")) {
+        return res.status(403).send({ message: 'InvalidTokenFormat' });
     }
 
-    req.user = payload;
+    token = token.replace("Bearer ", ""); // Delete "Bearer "
 
-    next();
-}
+    var segments = token.split('.');
+    
+    if (segments.length !== 3) {
+        return res.status(403).send({ message: 'InvalidToken' });
+    }
+
+    try {
+        var payload = jwt.decode(token, secret);
+
+        if (!payload || !payload.exp) {
+            return res.status(403).send({ message: 'MalformedToken' });
+        }
+
+        if (payload.exp <= moment().unix()) {
+            return res.status(403).send({ message: 'TokenExpired' });
+        }
+
+        req.user = payload;
+        next();
+    } catch (error) {
+        console.log("Error decoding token:", error);
+        return res.status(403).send({ message: 'ErrorToken' });
+    }
+};
