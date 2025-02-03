6a0705b5d7e4fb72ce110212e87f1811ddfa95ab
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-03 16:22:22 -0500

    Create main.py

---
+++ UserDomain/ValidateCodeService/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from routes.validate_code import router as validate_code_router
+import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+# Registrar rutas
+app.include_router(validate_code_router, prefix="/api")
+
+@app.get("/")
+def home():
+    return {"message": "Validate Code Service is running"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5060)
