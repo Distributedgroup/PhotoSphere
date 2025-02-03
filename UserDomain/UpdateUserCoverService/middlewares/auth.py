a60d8446e29bdcb61772cc908732fab79c8b1957
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-03 18:24:57 -0500

    Create auth.py

---
+++ UserDomain/UpdateUserCoverService/middlewares/auth.py
@@ -0,0 +1,28 @@
+import jwt
+import datetime
+from fastapi import HTTPException, Request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY", "6M5X#D6%7Nh*!pkR3HL7F@Fdx")
+
+async def auth(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header:
+        raise HTTPException(status_code=403, detail="NoHeadersError")
+
+    token = auth_header.replace('"', '').split(" ")[1]
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        print("🔍 Token data:", payload)  # <-- Add this for debugging
+
+        if payload["exp"] < datetime.datetime.utcnow().timestamp():
+            raise HTTPException(status_code=403, detail="TokenExpired")
+
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=403, detail="TokenExpired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=403, detail="InvalidToken")
