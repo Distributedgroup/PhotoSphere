50f4b9ecd138fa84de754116549e4d25fd976731
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-02 22:26:49 -0500

    Create jwt.php

---
+++ UserDomain/GetUserService/helpers/jwt.php
@@ -0,0 +1,18 @@
+<?php
+use Firebase\JWT\JWT;
+
+class JWTHandler {
+    public static function createToken($user) {
+        $payload = [
+            "sub" => $user["_id"],
+            "nombres" => $user["names"],
+            "apellidos" => $user["surnames"],
+            "email" => $user["email"],
+            "iat" => time(),
+            "exp" => time() + (30 * 24 * 60 * 60) // 30 days
+        ];
+
+        return JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256');
+    }
+}
+?>
