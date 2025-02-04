e28a8cd447d6ee6edb0a903727449cced21a1a3a
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-04 12:59:01 -0500

    Update app.js

--- UserDomain/UpdateUserService/app.js
+++ UserDomain/UpdateUserService/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 // **Connecting to MongoDB in Docker inside EC2**
-const MONGO_URI = process.env.MONGO_URI || "mongodb://admin:secret@ec2-18-207-77-6.compute-1.amazonaws.com:27017/userservice?authSource=admin";
+const MONGO_URI = process.env.MONGO_URI || "MONGO_URI=mongodb://54.205.9.185:27017/userservice";
 
 mongoose
     .connect(MONGO_URI, {
