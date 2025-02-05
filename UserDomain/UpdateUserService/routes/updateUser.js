16c7f825057389ee9558c5176768231bdcba887f
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-05 17:02:20 -0500

    Update updateUser.js

--- UserDomain/UpdateUserService/routes/updateUser.js
+++ UserDomain/UpdateUserService/routes/updateUser.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var updateUserController = require('../Controller/updateUserController');
-
+var auth = require('../middlewares/auth');
 var multiparty = require('connect-multiparty');
 var path = multiparty({uploadDir:'./uploads/stories'});
 
 
 var app = express.Router();
 
-app.put('/update_user/:id',updateUserController.update_user);
+app.put('/update_user/:id',auth.auth,updateUserController.update_user);
 
 module.exports = app;
