52e9aa1485fdb60e3b1393ade3cd777e0e02632e
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-06 19:21:45 -0500

    Update nav.component.ts

--- front/src/app/components/nav/nav.component.ts
+++ front/src/app/components/nav/nav.component.ts
@@ -24,7 +24,7 @@ export class NavComponent implements OnInit {
   public url = GLOBAL.url;
 
   constructor(
-    private _usuarioService:UserService,
+    private _UserService:UserService,
     private _router:Router,
     private _route:ActivatedRoute,
     private _postService:PostService
@@ -91,7 +91,7 @@ export class NavComponent implements OnInit {
 
   init_invitaciones(){
     this.load_invitacion = true;
-    this._usuarioService.get_invitaciones_usuario('Limite',this.token).subscribe(
+    this._UserService.get_invitaciones_usuario('Limite',this.token).subscribe(
       response=>{
         this.invitaciones = response.data;
         this.load_invitacion = false;
@@ -100,7 +100,7 @@ export class NavComponent implements OnInit {
   }
 
   set_invitacion(tipo:any,id:any,item:any){
-    this._usuarioService.aceptar_denegar_invitacion(tipo,id,this.token).subscribe(
+    this._UserService.aceptar_denegar_invitacion(tipo,id,this.token).subscribe(
       response=>{
         console.log(response);
         this.socket.emit('set-invitacion',{
