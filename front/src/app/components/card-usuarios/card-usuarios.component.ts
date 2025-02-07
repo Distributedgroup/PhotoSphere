9e07a2f8d615b6f769dbe7922878c32ef176d75f
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-06 19:19:29 -0500

    Update card-usuarios.component.ts

--- front/src/app/components/card-usuarios/card-usuarios.component.ts
+++ front/src/app/components/card-usuarios/card-usuarios.component.ts
@@ -1,66 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { HistoriaService } from 'src/app/services/historia.service';
 import { UserService } from 'src/app/services/user.service';
-declare var e:any;
-import { io } from "socket.io-client";
-import { GLOBAL } from 'src/app/services/GLOBAL';
-declare var $:any;
-declare var tns:any;
 
 @Component({
-  selector: 'app-card-usuarios',
-  templateUrl: './card-usuarios.component.html',
-  styleUrls: ['./card-usuarios.component.css']
+  selector: 'app-configuracion-usuario',
+  templateUrl: './configuracion-usuario.component.html',
+  styleUrls: ['./configuracion-usuario.component.css']
 })
-export class CardUsuariosComponent implements OnInit {
+export class ConfiguracionUsuarioComponent implements OnInit {
 
   public token = localStorage.getItem('token');
-  public user : any = {};
-  public socket = io("http://localhost:4201",{transports: ['websocket']});
-  public usuarios : Array<any> = [];
-  public url = GLOBAL.url;
+  public user :any = {};
+  public usuario : any = {
+    genero: '',
+    descripcion: '',
+  };
+  public msm_succes = '';
 
   constructor(
-    private _historiaService:HistoriaService,
     private _UserService:UserService
-  ) { 
-
-  }
+  ) { }
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('user')!);
-    
-    this.socket.on('set-new-invitacion',function(data:any){
-      console.log(data);
-      if(data.origen.toString() == this.user._id || data.destinario.toString() == this.user._id){
-        this.init_usuario();
-      }
-    }.bind(this));
-    
-    
     this.init_usuario();
   }
-  init_usuario(){
-    this._UserService.get_usuario_random(this.token).subscribe(
-      response=>{
-        this.usuarios = response.data;
 
-        for(var item of this.usuarios){
-          if(item.avatar == 'defecto.png') item.avatar = 'assets/images/usuario.png';
-          else if(item.avatar != 'defecto.png') item.avatar = this.url + 'obtener_portada_img/'+item.avatar;
-        }
-        
+  init_usuario(){
+    this._UserService.get_usuario(this.user._id,this.token).subscribe(
+      response=>{
+        this.usuario = response.data;
+        if(!this.usuario.genero) this.usuario.genero = '';
+        if(!this.usuario.descripcion) this.usuario.descripcion = '';
+  
       }
     );
   }
 
-  send_invitacion(id:any){
-    this._UserService.send_invitacion_amistad({
-      usuario_destinatario: id
-    },this.token).subscribe(
+  validate_descripcion(){
+   if(this.usuario.descripcion.length > 300) this.usuario.descripcion = this.usuario.descripcion.substring(0,300);
+  }
+
+  update(){
+    console.log(this.usuario);
+    this._UserService.update_user(this.usuario._id,this.usuario,this.token).subscribe(
       response=>{
         console.log(response);
-        this.socket.emit('send-invitacion',{_id:id});
+        if(response.data != undefined){
+          this.msm_succes = 'Se actualizó los datos de la cuenta';
+        }
       }
     );
   }
