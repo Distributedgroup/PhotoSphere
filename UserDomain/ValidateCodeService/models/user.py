c28f1c77672519a1444156ca0748754f55af8f6f
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-10 18:34:52 -0500

    Update user.py

--- UserDomain/ValidateCodeService/models/user.py
+++ UserDomain/ValidateCodeService/models/user.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:secret@ec2-18-207-77-6.compute-1.amazonaws.com:27017/userservice?authSource=admin")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://52.1.158.25:27017/userservice")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.userservice
