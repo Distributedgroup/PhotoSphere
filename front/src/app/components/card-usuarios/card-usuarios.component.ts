fa886fc0ec87089b02d894362f2d79f2211baa04
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-11 11:37:28 -0500

    Update card-usuarios.component.ts

--- front/src/app/components/card-usuarios/card-usuarios.component.ts
+++ front/src/app/components/card-usuarios/card-usuarios.component.ts
@@ -16,7 +16,7 @@ export class CardUsuariosComponent implements OnInit {
 
   public token = localStorage.getItem('token');
   public user : any = {};
-  public socket = io("http://localhost:4201",{transports: ['websocket']});
+  public socket = io("http://54.172.88.33",{transports: ['websocket']});
   public usuarios : Array<any> = [];
   public url = GLOBAL.url;
 
