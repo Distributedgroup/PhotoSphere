da61290516d0f6848d69ef07a68b81d552ebfe70
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-11 16:06:53 -0500

    Update Post.js

--- PostDomain/CreatePostService/Models/Post.js
+++ PostDomain/CreatePostService/Models/Post.js
@@ -1,5 +1,10 @@
+const connections = require('../app');
+const mongoConn2 = connections.mongoConn2;
 const mongoose = require('mongoose');
-const { mongoConn2 } = require('../app'); // Importa mongoConn2 correctamente
+
+if (!mongoConn2) {
+    throw new Error("❌ Error: mongoConn2 no está definido en Post.js");
+}
 
 const Schema = mongoose.Schema;
 
@@ -7,7 +12,7 @@ const PostSchema = new Schema({
     extract: { type: String, required: true },
     content: { type: String, required: true },
     media: { type: String, required: false },
-    type: { type: String, required: true }, // TEXTO, MEDIA, GRUPO
+    type: { type: String, required: true },
     privacy: { type: String, required: true },
     user: { type: Schema.ObjectId, ref: 'user', required: true },
     createdAt: { type: Date, default: Date.now },
