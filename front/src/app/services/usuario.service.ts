3056e7e3c1a94025bfdd8c384f28be3d11d1a83d
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-11 16:26:00 -0500

    Update usuario.service.ts

--- front/src/app/services/usuario.service.ts
+++ front/src/app/services/usuario.service.ts
@@ -26,16 +26,16 @@ export class UsuarioService {
     return this._http.post(this.url+'login_user',data,{headers:headers})
   }
 
-get_user(id: any, token: any): Observable<any> {
-    let headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` });
-    return this._http.get(`${this.url}/get_user/${id}`, { headers: headers });
-}
+  get_user(id:any,token:any):Observable<any>{
+    let headers = new HttpHeaders({'Content-Type':'application/json','Authorization':token});
+    return this._http.get(this.url+'get_user/'+id,{headers:headers})
+  }
 
   update_user(id:any,data:any,token:any):Observable<any>{
     let headers = new HttpHeaders({'Content-Type':'application/json','Authorization':token});
     return this._http.put(this.url+'update_user/'+id,data,{headers:headers})
   }
-
+  
   update_password(id:any,data:any,token:any):Observable<any>{
     let headers = new HttpHeaders({'Content-Type':'application/json','Authorization':token});
     return this._http.put(this.url+'update_password/'+id,data,{headers:headers})
