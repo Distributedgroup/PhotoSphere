d181adb6ae78170f789cd4ddba76bd1489824205
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2025-02-11 10:21:03 -0500

    Update User.php

--- UserDomain/GetUserService/Model/User.php
+++ UserDomain/GetUserService/Model/User.php
@@ -1,8 +1,47 @@
 <?php
-class User {
-    public static function findById($id, $db) {
-        $collection = $db->users;
-        return $collection->findOne(['_id' => new MongoDB\BSON\ObjectId($id)]);
-    }
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'names',
+        'surnames',
+        'email',
+        'country',
+        'profession',
+        'birth',
+        'gender',
+        'phone',
+        'avatar',
+        'frontPage',
+        'state',
+        'description',
+        'username',
+        'password',
+        'code_reset',
+    ];
+
+    protected $attributes = [
+        'avatar' => 'defecto.png',
+        'state' => false,
+    ];
+
+    protected $hidden = [
+        'password',
+        'code_reset',
+    ];
+
+    protected $casts = [
+        'birth' => 'date',
+        'state' => 'boolean',
+        'created_at' => 'datetime',
+    ];
 }
-?>
