41f8a443a433e29051b083372d18e1d58538e74b
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-11 15:15:25 -0500

    Update Post.js

--- PostDomain/CreatePostService/Models/Post.js
+++ PostDomain/CreatePostService/Models/Post.js
@@ -1,14 +1,15 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const { mongoConn1 } = require('../app'); // Importar la conexión correcta
+const Schema = mongoose.Schema;
 
-var PostSchema = Schema({
-    extract: {type: String, required: true},
-    content: {type: String, required: true},
-    media: {type: String, required: false},
-    type: {type: String, required: true}, //TEXTO,MEDIA,GRUPO
-    privacy: {type: String, required: true}, 
-    user: {type: Schema.ObjectId, ref: 'user', required: true},
-    createdAt: {type: Date, default: Date.now},
+const PostSchema = new Schema({
+    extract: { type: String, required: true },
+    content: { type: String, required: true },
+    media: { type: String, required: false },
+    type: { type: String, required: true }, // TEXTO, MEDIA, GRUPO
+    privacy: { type: String, required: true },
+    user: { type: Schema.ObjectId, ref: 'user', required: true },
+    createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('post',PostSchema);
+module.exports = mongoConn1.model('post', PostSchema);
