6769951a33c7270c1128d36aa072c4e347f5cfdb
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-11 15:19:55 -0500

    Update createPostController.js

--- PostDomain/CreatePostService/Controller/createPostController.js
+++ PostDomain/CreatePostService/Controller/createPostController.js
@@ -1,49 +1,49 @@
-var Post = require('../models/Post');
-var User_friend = require('../models/User_friend');
-const Notification = require('../models/Notification');
-var fs = require('fs');
-var path = require('path');
+const { mysqlPool } = require('../app'); // Importar conexión MySQL
+const Post = require('../models/Post'); // Usa MongoDB `socialN`
+const Notification = require('../models/Notification'); // Usa MongoDB `socialP`
+const { getFriends } = require('../models/User_Friend'); // Funciones para MySQL
+const fs = require('fs');
+const path = require('path');
 
-
-const create_post = async function(req,res){
+const create_post = async function(req, res) {
     if (req.user) {
-       try {
+        try {
             let data = req.body;
 
-            if(data.tipo == 'Media'){
+            if (data.tipo === 'Media') {
                 let img_path = req.files.media.path.split('\\')[2];
                 data.media = img_path;
             }
 
             data.user = req.user.sub;
+
+            // Crear post en MongoDB `socialN`
             let post = await Post.create(data);
 
-            //Notificacion
-            let friends = await User_friend.find({user_origin:req.user.sub}).populate('user_friend');
+            // Obtener amigos desde MySQL
+            let friends = await getFriends(req.user.sub);
 
-            for(var item of friends){
-                let description = req.user.names.split(' ')[0] + ' ' + req.user.surnames.split(' ')[0] + ' a creado una nueva publicación';
+            for (let item of friends) {
+                let description = `${req.user.names.split(' ')[0]} ${req.user.surnames.split(' ')[0]} ha creado una nueva publicación`;
 
+                // Crear notificación en MongoDB `socialP`
                 await Notification.create({
                     type: 'Publicaciones',
                     description,
                     user_interaction: req.user.sub,
-                    user: item.user_friend._id,
+                    user: item.user_friend,
                     post: post._id
                 });
             }
 
-            res.status(200).send({data:post,friends});
-       } catch (error) {
+            res.status(200).send({ data: post, friends });
+        } catch (error) {
             console.log(error);
-            res.status(200).send({data:undefined,message:'No se pudo crear la publicación.'});
-       }
-
+            res.status(500).send({ data: undefined, message: 'No se pudo crear la publicación.' });
+        }
     } else {
-        res.status(403).send({message: 'NoAccess'}); 
+        res.status(403).send({ message: 'NoAccess' });
     }
-}
+};
 
-module.exports = {
-    create_post
-}
+module.exports = { create_post };
