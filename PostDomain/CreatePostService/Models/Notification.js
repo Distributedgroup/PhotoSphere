f4d52e48f6d61b5c5e0b82e3379a3618629dda0d
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-11 15:15:10 -0500

    Update Notification.js

--- PostDomain/CreatePostService/Models/Notification.js
+++ PostDomain/CreatePostService/Models/Notification.js
@@ -1,17 +1,16 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const { mongoConn2 } = require('../app'); // Importar la conexión correcta
+const Schema = mongoose.Schema;
 
-var NotificationSchema = Schema({
-    type: {type: String, required: true}, //Publicaciones - Solicitudes de amistad
-    description: {type: String, required: true},
-    state: {type: Boolean, default: false, required: true}, 
-    user: {type: Schema.ObjectId, ref: 'user', required: true},
-    user_interaction: {type: Schema.ObjectId, ref: 'user', required: true},
-
-    post: {type: Schema.ObjectId, ref: 'post', required: false},
-    user_friend: {type: Schema.ObjectId, ref: 'user_friend', required: false},
-
-    createdAt: {type: Date, default: Date.now},
+const NotificationSchema = new Schema({
+    type: { type: String, required: true }, // Publicaciones - Solicitudes de amistad
+    description: { type: String, required: true },
+    state: { type: Boolean, default: false, required: true },
+    user: { type: Schema.ObjectId, ref: 'user', required: true },
+    user_interaction: { type: Schema.ObjectId, ref: 'user', required: true },
+    post: { type: Schema.ObjectId, ref: 'post', required: false },
+    user_friend: { type: Schema.ObjectId, ref: 'user_friend', required: false },
+    createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('notification',NotificationSchema);
+module.exports = mongoConn2.model('notifications', NotificationSchema);
