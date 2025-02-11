30bb7c16ff1422880709a897a6e76ff4beda6ebe
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-11 18:30:20 -0500

    Update jwt.js

--- PostDomain/GetNotificationsService/helpers/jwt.js
+++ PostDomain/GetNotificationsService/helpers/jwt.js
@@ -2,14 +2,15 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var secret = '6M5X#D6%7Nh*!pkR3HL7F@Fdx';
 
-exports.createToken = function(usuario){
+exports.createToken = function(user){
     var payload = {
-        sub: usuario._id,
-        nombres: usuario.nombres,
-        apellidos: usuario.apellidos,
-        email: usuario.email,
+        sub: user._id,
+        names: user.names,
+        surnames: user.surnames,
+        email: user.email,
         iat: moment().unix(),
         exp: moment().add(30,'day').unix(),
+
     }
 
     return jwt.encode(payload,secret);
