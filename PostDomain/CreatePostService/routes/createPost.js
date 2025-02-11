33ada1aeb20c8c6ab6a8d62e29e62579fe7d6e73
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-11 15:46:19 -0500

    Update createPost.js

--- PostDomain/CreatePostService/routes/createPost.js
+++ PostDomain/CreatePostService/routes/createPost.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var createPostController = require('../controller/createPostController');
+var createPostController = require('../Controller/createPostController');
 var auth = require('../middlewares/auth');
 var multiparty = require('connect-multiparty');
 var path = multiparty({uploadDir:'../uploads/posts'});
