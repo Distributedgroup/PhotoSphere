5e796dc375e0766fb74bbd639ca5852fa29fc4d1
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 14:43:37 -0500

    Update auth.js

--- UserDomain/GetUserService/middlewares/auth.js
+++ UserDomain/GetUserService/middlewares/auth.js
@@ -2,28 +2,31 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var secret = '6M5X#D6%7Nh*!pkR3HL7F@Fdx';
 
-exports.auth = function(req,res,next){
-    if(!req.headers.authorization){
-        return res.status(403).send({message: 'NoHeadersError'}); 
+exports.auth = function(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(403).send({ message: 'NoHeadersError' });
     }
 
-    var token = req.headers.authorization.replace(/['"]+/g,'');
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+    console.log("Token recibido:", token);  // 🔍 Imprime el token en los logs
 
-    var segment = token.split('.');
-    
-    if(segment.length != 3){
-        return res.status(403).send({message: 'InvalidToken'}); 
-    }else{
-        try {
-            var payload = jwt.decode(token,secret);
-            if(payload.exp <= moment().unix()) return res.status(403).send({message: 'TokenExpirado'}); 
-        } catch (error) {
-            console.log(error);
-            return res.status(403).send({message: 'ErrorToken'}); 
+    var segments = token.split('.');
+    if (segments.length !== 3) {
+        return res.status(403).send({ message: 'InvalidToken' });
+    }
+
+    try {
+        var payload = jwt.decode(token, secret);
+        console.log("Payload decodificado:", payload);  // 🔍 Verifica el contenido del token
+
+        if (payload.exp <= moment().unix()) {
+            return res.status(403).send({ message: 'TokenExpirado' });
         }
+    } catch (error) {
+        console.log("Error al decodificar el token:", error);  // 🔍 Muestra el error exacto
+        return res.status(403).send({ message: 'ErrorToken', error: error.message });
     }
 
     req.user = payload;
-
     next();
-}
+};
