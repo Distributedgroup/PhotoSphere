64383cea2478fcf1b72a2f5fabf248dd5ca6fe2a
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 12:24:29 -0500

    Update getUserUsername.js

--- UserDomain/GetUserUsernameService/routes/getUserUsername.js
+++ UserDomain/GetUserUsernameService/routes/getUserUsername.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var historiasController = require('../Controllers/getUserUsernameController');
+var historiasController = require('../Controller/getUserUsernameController');
 var auth = require('../middlewares/auth');
 var multiparty = require('connect-multiparty');
 var path = multiparty({uploadDir:'./uploads/stories'});
