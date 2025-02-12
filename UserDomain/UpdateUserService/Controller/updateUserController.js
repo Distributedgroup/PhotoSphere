5a99bd61d481a59c4c8f657a52e9748346ba6e2f
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 16:11:07 -0500

    Update updateUserController.js

--- UserDomain/UpdateUserService/Controller/updateUserController.js
+++ UserDomain/UpdateUserService/Controller/updateUserController.js
@@ -1,36 +1,29 @@
 const User = require('../Model/User'); // Ensure the model is imported correctly.
 
-const update_user = async function(req, res) {
-    try {
-        const id = req.params['id'];
-        const data = req.body;
+const update_user = async function(req,res){
+    if (req.user) {
+        var id = req.params['id'];
+        var data = req.body;
 
-        // Check if the user exists
-        let userExists = await User.findById(id);
-        if (!userExists) {
-            return res.status(404).send({ message: "User not found" });
-        }
+        console.log(data);
 
-        console.log("Data received for update:", data);
+        var user = await User.findByIdAndUpdate({_id:id},{
+            names: data.names,
+            surnames: data.surnames,
+            genrer: data.genrer,
+            birth: data.birth,
+            profession: data.profession,
+            phone: data.telephone,
+            description: data.description
+        });
 
-        // Update user
-        let user = await User.findByIdAndUpdate(id, {
-            names: data.names || userExists.names,
-            surnames: data.surnames || userExists.surnames,
-            gender: data.gender || userExists.gender,
-            birth: data.birth || userExists.birth,
-            profession: data.profession || userExists.profession,
-            phone: data.phone || userExists.phone,
-            description: data.description || userExists.description
-        }, { new: true }); // `new: true` returns the updated document
-
-        res.status(200).send({ message: "User updated successfully", data: user });
-    } catch (error) {
-        console.error("Error updating user:", error);
-        res.status(500).send({ message: "Server error", error });
+        res.status(200).send({data:user});
+    } else {
+        res.status(403).send({message: 'NoAccess'}); 
     }
-};
+}
+
 
 module.exports = {
     update_user
-};
+}
