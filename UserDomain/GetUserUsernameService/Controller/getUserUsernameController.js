6a17019f8d9113eed40f841a2fc8a4dfc1f2a2ca
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 12:07:52 -0500

    Update getUserUsernameController.js

--- UserDomain/GetUserUsernameService/Controller/getUserUsernameController.js
+++ UserDomain/GetUserUsernameService/Controller/getUserUsernameController.js
@@ -1,23 +1,37 @@
-const get_user_username = async function(req,res){
+const User = require('../models/User');
+const { getMySQLPool } = require('../database');
+
+const get_user_username = async function (req, res) {
     if (req.user) {
-        let username = req.params['username'];
-
-        var users = await User.find({username:username});
-       
-
-        if(users.length >= 1){
-            var friends = await User_friend.find({user_origin:users[0]._id});
-            res.status(200).send({data:users[0],n_friends:friends.length});
-        }else{
-            res.status(200).send({data:undefined});
+        try {
+            let username = req.params['username'];
+            
+            // Buscar usuario en MongoDB
+            let users = await User.find({ username: username });
+            
+            if (users.length >= 1) {
+                const userId = users[0]._id;
+                
+                // Buscar amigos en MySQL
+                const mysqlPool = await getMySQLPool();
+                const connection = await mysqlPool.getConnection();
+                const [friends] = await connection.query(
+                    "SELECT COUNT(*) AS n_friends FROM user_friend WHERE user_origin = ?",
+                    [userId]
+                );
+                connection.release();
+                
+                res.status(200).send({ data: users[0], n_friends: friends[0].n_friends });
+            } else {
+                res.status(200).send({ data: undefined });
+            }
+        } catch (error) {
+            console.error("❌ Error en get_user_username:", error);
+            res.status(500).send({ message: 'Error en el servidor' });
         }
-
-        
     } else {
-        res.status(403).send({message: 'NoAccess'}); 
+        res.status(403).send({ message: 'NoAccess' });
     }
-}
+};
 
-module.exports = {
-    get_user_username
-}
+module.exports = { get_user_username };
