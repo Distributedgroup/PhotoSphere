d994dfb2689fa8672412effebaa9acee2a7529f4
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 17:27:06 -0500

    Update User.js

--- UserDomain/GetUserUsernameService/Model/User.js
+++ UserDomain/GetUserUsernameService/Model/User.js
@@ -1,27 +1,26 @@
-const { mongoConn1 } = require('../app');  // Importa correctamente `mongoConn1`
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    names: { type: String, required: true },
-    surnames: { type: String, required: true },
-    email: { type: String, required: true },
-    country: { type: String, required: false },
-    profession: { type: String, required: false },
-    birth: { type: Date, required: false },
-    gender: { type: String, require: false },
-    phone: { type: String, required: false },
-    avatar: { type: String, default: 'defecto.png', required: false },
-    frontPage: { type: String, required: false },
-    state: { type: Boolean, default: false, required: false },
-    description: { type: String, required: false },
-    username: { type: String, required: false },
-    password: { type: String, required: true },
-    code_reset: { type: String, required: false },
-    createdAt: { type: Date, default: Date.now },
+var UserSchema = Schema({
+    names: {type: String, required: true},
+    surnames: {type: String, required: true},
+    email: {type: String, required: true},
+    country: {type: String, required: false},
+    profession: {type: String, required: false},
+    birth: {type: Date, required: false},
+    gender:{type:String, require:false},
+    phone: {type: String, required: false},
+    avatar: {type: String, default: 'defecto.png', required: false},
+    frontPage: {type: String, required: false},
+
+    state: {type: Boolean, default: false, required: false},
+    description: {type: String, required: false},
+    username: {type: String, required: false},
+    password: {type: String, required: true},
+    code_reset: {type: String, required: false},
+    createdAt: {type: Date, default: Date.now},
+
+
 });
 
-// 🔹 Usa `mongoConn1` para definir el modelo
-const User = mongoConn1.model('User', UserSchema);
-
-module.exports = User;
+module.exports = mongoose.model('user',UserSchema);
