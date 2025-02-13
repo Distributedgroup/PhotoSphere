7450253ec0062128213d6cd7c90d40df39b2ff97
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-13 13:05:19 -0500

    Update User_invitation.js

--- UserDomain/GetUserInvitationsService/Model/User_invitation.js
+++ UserDomain/GetUserInvitationsService/Model/User_invitation.js
@@ -1,5 +1,21 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const MONGO_URI_2 = "mongodb://3.220.140.139:27017/userinvitation"; // IP de la segunda base de datos
+const mongoConn2 = mongoose.createConnection(MONGO_URI_2, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoConn2.on('error', err => {
+    console.error("❌ Error conectando a MongoDB 2 en User_invitation.js:", err);
+});
+
+mongoConn2.once('open', () => {
+    console.log("✅ Conectado correctamente a MongoDB 2 en User_invitation.js");
+});
+
+
+const Schema = mongoose.Schema;
 
 var User_invitationSchema = Schema({
     user_origin: { type: Schema.ObjectId, ref: 'user', require: true },
@@ -7,4 +23,4 @@ var User_invitationSchema = Schema({
     createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('user_invitation', User_invitationSchema);
+module.exports = mongoConn2.model('user_invitation', User_invitationSchema);
