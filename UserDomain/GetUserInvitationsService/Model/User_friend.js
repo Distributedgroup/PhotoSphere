3af0a4a1b0fb5d3e2d34c3d2f2c3e60a193825fb
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-13 12:15:40 -0500

    Update User_friend.js

--- UserDomain/GetUserInvitationsService/Model/User_friend.js
+++ UserDomain/GetUserInvitationsService/Model/User_friend.js
@@ -1,11 +1,26 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const MONGO_URI_2 = "mongodb://3.220.140.139:27017/userinvitation"; // IP de la segunda base de datos
+const mongoConn2 = mongoose.createConnection(MONGO_URI_2, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoConn2.on('error', err => {
+    console.error("❌ Error conectando a MongoDB 2 en User_invitation.js:", err);
+});
+
+mongoConn2.once('open', () => {
+    console.log("✅ Conectado correctamente a MongoDB 2 en User_invitation.js");
+});
 
 
-var User_friendSchema = Schema({
+const Schema = mongoose.Schema;
+
+var User_invitationSchema = Schema({
     user_origin: { type: Schema.ObjectId, ref: 'user', require: true },
-    user_friend: { type: Schema.ObjectId, ref: 'user', require: true },
+    recipient_user: { type: Schema.ObjectId, ref: 'user', require: true },
     createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('user_friend', User_friendSchema);
+module.exports = mongoConn2.model('user_invitation', User_invitationSchema);
