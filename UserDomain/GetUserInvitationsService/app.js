d1edb6684accdfad820cb12441713a2450e5511f
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-13 12:32:16 -0500

    Update app.js

--- UserDomain/GetUserInvitationsService/app.js
+++ UserDomain/GetUserInvitationsService/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyparser.json({ limit: '50mb', extended: true }));
 
-const userRoutes = require('./routes/getUserInvitation');
+const userRoutes = require('./routes/getUserInvitations');
 app.use(express.json());
 app.use("/api", userRoutes);
 
