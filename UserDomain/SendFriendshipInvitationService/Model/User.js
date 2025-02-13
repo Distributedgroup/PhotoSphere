f188de86e21de77f49c3b476a8a4129d31a70110
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 23:27:00 -0500

    Update User.js

--- UserDomain/SendFriendshipInvitationService/Model/User.js
+++ UserDomain/SendFriendshipInvitationService/Model/User.js
@@ -1,6 +1,21 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
+const MONGO_URI_1 = "mongodb://52.1.158.25:27017/userservice"; // IP de la segunda base de datos
+const mongoConn1 = mongoose.createConnection(MONGO_URI_1, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoConn1.on('error', err => {
+    console.error("❌ Error conectando a MongoDB 1 en User.js:", err);
+});
+
+mongoConn1.once('open', () => {
+    console.log("✅ Conectado correctamente a MongoDB 1 en User.js");
+});
+
+
+const Schema = mongoose.Schema;
 var UserSchema = Schema({
     names: {type: String, required: true},
     surnames: {type: String, required: true},
