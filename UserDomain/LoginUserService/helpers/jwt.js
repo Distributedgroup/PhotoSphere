f2553fd05df4886a5820aa3fd405026689a7d25a
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-12 19:10:45 -0500

    Update jwt.js

--- UserDomain/LoginUserService/helpers/jwt.js
+++ UserDomain/LoginUserService/helpers/jwt.js
@@ -1,16 +1,17 @@
-var jwt = require('jwt-simple');
+var jwt = require('jsonwebtoken');
 var moment = require('moment');
+
 var secret = '6M5X#D6%7Nh*!pkR3HL7F@Fdx';
 
-exports.createToken = function(user){
+exports.createToken = function(user) {
     var payload = {
         sub: user._id,
-        nombres: user.nombres,
-        apellidos: user.apellidos,
+        names: user.names || "SinNombre",
+        surnames: user.surnames || "SinApellido",
         email: user.email,
         iat: moment().unix(),
-        exp: moment().add(30,'day').unix(),
-    }
+        exp: moment().add(30, 'days').unix()
+    };
 
-    return jwt.encode(payload,secret);
-}
+    return jwt.sign(payload, secret);
+};
