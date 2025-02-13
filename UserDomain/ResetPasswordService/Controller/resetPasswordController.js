c32e87900ef9ce769e84a0899d93b2b9f0b2f31c
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-13 11:13:25 -0500

    Update resetPasswordController.js

--- UserDomain/ResetPasswordService/Controller/resetPasswordController.js
+++ UserDomain/ResetPasswordService/Controller/resetPasswordController.js
@@ -1,22 +1,41 @@
 var User = require('../Model/User'); 
 const bcrypt = require('bcrypt'); 
 
-const reset_password = async function(req,res){
-    var email = req.params['email'];
-    var data = req.body;
+const saltRounds = 10; // 🔹 Definir el número de rondas para generar el salt
 
-    var user = await User.findOne({email:email});
+const reset_password = async function(req, res) {
+    try {
+        var email = req.params['email'];
+        var data = req.body;
 
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(data.password_new, salt, async function(err, hash) {
-            await User.findByIdAndUpdate({_id:user._id},{
-                password: hash
+        var user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.status(404).send({ message: 'Usuario no encontrado' });
+        }
+
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) {
+                return res.status(500).send({ message: 'Error generando salt', error: err });
+            }
+
+            bcrypt.hash(data.password_new, salt, async function(err, hash) {
+                if (err) {
+                    return res.status(500).send({ message: 'Error generando hash', error: err });
+                }
+
+                await User.findByIdAndUpdate(user._id, {
+                    password: hash
+                });
+
+                res.status(200).send({ message: 'Contraseña actualizada correctamente' });
             });
-            res.status(200).send({data:user});
         });
-    });
+    } catch (error) {
+        res.status(500).send({ message: 'Error en el servidor', error });
+    }
 }
 
 module.exports = {
     reset_password
-}
+};
