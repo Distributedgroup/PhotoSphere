a14fa22f50a930f7b7ba0c0fbfe00d6d35abfcf6
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-02-13 00:49:32 -0500

    Update sendFriendshipInvitationController.js

--- UserDomain/SendFriendshipInvitationService/Controller/sendFriendshipInvitationController.js
+++ UserDomain/SendFriendshipInvitationService/Controller/sendFriendshipInvitationController.js
@@ -1,16 +1,20 @@
 
 const User_invitation = require('../Model/User_invitation'); 
 
-const send_friendship_invitation = async function(req,res){
+
+
+const send_friendship_invitation = async function(req, res) {
+    console.log("Solicitud recibida:", req.body);  // Para ver si la solicitud llega
     if (req.user) {
         let data = req.body;
         data.user_origin = req.user.sub;
         let invitation = await User_invitation.create(data);
-        res.status(200).send({data:invitation});
+        res.status(200).send({ data: invitation });
     } else {
-        res.status(403).send({message: 'NoAccess'}); 
+        res.status(403).send({ message: 'NoAccess' });
     }
-}
+};
+
 
 module.exports = {
     send_friendship_invitation
